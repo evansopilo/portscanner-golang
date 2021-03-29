9b451aac3986b12127d40e8032970f1828e6c369
evans-opilo <79232316+evans-opilo@users.noreply.github.com>
2021-03-29 20:13:54 +0300

    Add files via upload

---
+++ portscan.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+	"time"
+)
+
+type ScanResult struct {
+	Port  int
+	State string
+}
+
+func ScanPort(protocol, hostname string, port int) ScanResult {
+	result := ScanResult{Port: port}
+	address := hostname + ":" + strconv.Itoa(port)
+	conn, err := net.DialTimeout(protocol, address, 60*time.Second)
+
+	if err != nil {
+		result.State = "Closed"
+		return result
+	}
+	defer conn.Close()
+	result.State = "Open"
+	return result
+}
+
+func InitialScan(hostname string) []ScanResult {
+
+	var results []ScanResult
+
+	for i := 0; i <= 1024; i++ {
+		results = append(results, ScanPort("tcp", hostname, i))
+	}
+
+	return results
+}
+
+func main() {
+	fmt.Println("Port Scanning")
+	results := InitialScan("localhost")
+
+	for i := 0; i < len(results); i++ {
+		fmt.Println(results[i])
+	}
+
+}
